0a85f14bddfb10946cb0ad4ebfb0131919fed768
MaikolCode <maikol320434@gmail.com>
2021-04-07 20:24:00 -0500

    Finalizando vista

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect,useState } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
@@ -62,166 +63,166 @@ function App() {
   return (
     <div>
     <React.Fragment>
-    <CssBaseline />
-    <Container maxWidth="sm">
-      <div>
-        <Typography component="div" style={{ backgroundColor: 'rgba(6, 5, 66, 1)', height: '110vh', marginTop: '10px', borderRadius: '20px', marginBottom: '40px', padding: '20px', boxShadow: '2px 3px 2px 3px rgba(0, 0, 0, 0.4)'}}>
-          <div className={classes.root}>
-            <Grid container spacing={3}>
-              <Grid item xs={12}>
-                <Paper className={classes.paper}>
-                  <input className={classes.label} type="text" id="Name" name="Name" placeholder="Digite un numero"/>
-                </Paper>
+      <CssBaseline />
+        <Container maxWidth="sm">
+        <div>
+          <Typography component="div" style={{ backgroundColor: 'rgba(6, 5, 66, 1)', height: '110vh', marginTop: '10px', borderRadius: '20px', marginBottom: '40px', padding: '20px', boxShadow: '2px 3px 2px 3px rgba(0, 0, 0, 0.4)'}}>
+            <div className={classes.root}>
+              <Grid container spacing={3}>
+                <Grid item xs={12}>
+                  <Paper className={classes.paper}>
+                    <input className={classes.label} type="text" id="Name" name="Name" placeholder="Digite un numero"/>
+                  </Paper>
+                </Grid>
+                <Grid item xs={3}>
+                  <Paper className={classes.btn}>
+                    <Button variant="contained" color="secondary">
+                      <p className={classes.txt}>AC</p>
+                    </Button>
+                  </Paper>
+                </Grid>
+                <Grid item xs={3}>
+                  <Paper className={classes.btn}>
+                    <Button variant="contained">
+                      <p className={classes.txt}>CE</p>
+                    </Button>
+                  </Paper>
+                </Grid>
+                <Grid item xs={3}>
+                  <Paper className={classes.btn}>
+                    <Button variant="contained" color="primary">
+                      <p className={classes.txt}>%</p>
+                    </Button>
+                  </Paper>
+                </Grid>
+                <Grid item xs={3}>
+                  <Paper className={classes.btn}>
+                    <Button variant="contained" color="primary">
+                      <p className={classes.txt}>/</p>
+                    </Button>
+                  </Paper>
+                </Grid>
               </Grid>
-              <Grid item xs={3}>
-                <Paper className={classes.btn}>
-                  <Button variant="contained" color="secondary">
-                    <p className={classes.txt}>AC</p>
-                  </Button>
-                </Paper>
+              <Grid container spacing={3}>
+                <Grid item xs={3}>
+                  <Paper className={classes.btn}>
+                    <Button variant="contained" color="primary">
+                      <p className={classes.txt}>7</p>
+                    </Button>
+                  </Paper>
+                </Grid>
+                <Grid item xs={3}>
+                  <Paper className={classes.btn}>
+                    <Button variant="contained" color="primary">
+                      <p className={classes.txt}>8</p>
+                    </Button>
+                  </Paper>
+                </Grid>
+                <Grid item xs={3}>
+                  <Paper className={classes.btn}>
+                    <Button variant="contained" color="primary">
+                      <p className={classes.txt}>9</p>
+                    </Button>
+                  </Paper>
+                </Grid>
+                <Grid item xs={3}>
+                  <Paper className={classes.btn}>
+                    <Button variant="contained" color="primary">
+                      <p className={classes.txt}>x</p>
+                    </Button>
+                  </Paper>
+                </Grid>
               </Grid>
-              <Grid item xs={3}>
-                <Paper className={classes.btn}>
-                  <Button variant="contained">
-                    <p className={classes.txt}>CE</p>
-                  </Button>
-                </Paper>
+              <Grid container spacing={3}>
+                <Grid item xs={3}>
+                  <Paper className={classes.btn}>
+                    <Button variant="contained" color="primary">
+                      <p className={classes.txt}>4</p>
+                    </Button>
+                  </Paper>
+                </Grid>
+                <Grid item xs={3}>
+                  <Paper className={classes.btn}>
+                    <Button variant="contained" color="primary">
+                      <p className={classes.txt}>5</p>
+                    </Button>
+                  </Paper>
+                </Grid>
+                <Grid item xs={3}>
+                  <Paper className={classes.btn}>
+                    <Button variant="contained" color="primary">
+                      <p className={classes.txt}>6</p>
+                    </Button>
+                  </Paper>
+                </Grid>
+                <Grid item xs={3}>
+                  <Paper className={classes.btn}>
+                    <Button variant="contained" color="primary">
+                      <p className={classes.txt}>-</p>
+                    </Button>
+                  </Paper>
+                </Grid>
               </Grid>
-              <Grid item xs={3}>
-                <Paper className={classes.btn}>
-                  <Button variant="contained" color="primary">
-                    <p className={classes.txt}>%</p>
-                  </Button>
-                </Paper>
+              <Grid container spacing={3}>
+                <Grid item xs={3}>
+                  <Paper className={classes.btn}>
+                    <Button variant="contained" color="primary">
+                      <p className={classes.txt}>1</p>
+                    </Button>
+                  </Paper>
+                </Grid>
+                <Grid item xs={3}>
+                  <Paper className={classes.btn}>
+                    <Button variant="contained" color="primary">
+                      <p className={classes.txt}>2</p>
+                    </Button>
+                  </Paper>
+                </Grid>
+                <Grid item xs={3}>
+                  <Paper className={classes.btn}>
+                    <Button variant="contained" color="primary">
+                      <p className={classes.txt}>3</p>
+                    </Button>
+                  </Paper>
+                </Grid>
+                <Grid item xs={3}>
+                  <Paper className={classes.btn}>
+                    <Button variant="contained" color="primary">
+                      <p className={classes.txt}>+</p>
+                    </Button>
+                  </Paper>
+                </Grid>
               </Grid>
-              <Grid item xs={3}>
-                <Paper className={classes.btn}>
-                  <Button variant="contained" color="primary">
-                    <p className={classes.txt}>/</p>
-                  </Button>
-                </Paper>
+              <Grid container spacing={3}>
+                <Grid item xs={3}>
+                  <Paper className={classes.btn}>
+                    <Button variant="contained" color="primary">
+                      <p className={classes.txt}>0</p>
+                    </Button>
+                  </Paper>
+                </Grid>
+                <Grid item xs={3}>
+                  <Paper className={classes.btn}>
+                    <Button variant="contained" color="primary">
+                      <p className={classes.txt}>.</p>
+                    </Button>
+                  </Paper>
+                </Grid>
+                <Grid item xs={6}>
+                  <Paper className={classes.btn}>
+                    <Button style={{width: '100%'}} variant="contained" color="primary">
+                      <p className={classes.txt}>=</p>
+                    </Button>
+                  </Paper>
+                </Grid>
               </Grid>
-            </Grid>
-            <Grid container spacing={3}>
-              <Grid item xs={3}>
-                <Paper className={classes.btn}>
-                  <Button variant="contained" color="primary">
-                    <p className={classes.txt}>7</p>
-                  </Button>
-                </Paper>
-              </Grid>
-              <Grid item xs={3}>
-                <Paper className={classes.btn}>
-                  <Button variant="contained" color="primary">
-                    <p className={classes.txt}>8</p>
-                  </Button>
-                </Paper>
-              </Grid>
-              <Grid item xs={3}>
-                <Paper className={classes.btn}>
-                  <Button variant="contained" color="primary">
-                    <p className={classes.txt}>9</p>
-                  </Button>
-                </Paper>
-              </Grid>
-              <Grid item xs={3}>
-                <Paper className={classes.btn}>
-                  <Button variant="contained" color="primary">
-                    <p className={classes.txt}>x</p>
-                  </Button>
-                </Paper>
-              </Grid>
-            </Grid>
-            <Grid container spacing={3}>
-              <Grid item xs={3}>
-                <Paper className={classes.btn}>
-                  <Button variant="contained" color="primary">
-                    <p className={classes.txt}>4</p>
-                  </Button>
-                </Paper>
-              </Grid>
-              <Grid item xs={3}>
-                <Paper className={classes.btn}>
-                  <Button variant="contained" color="primary">
-                    <p className={classes.txt}>5</p>
-                  </Button>
-                </Paper>
-              </Grid>
-              <Grid item xs={3}>
-                <Paper className={classes.btn}>
-                  <Button variant="contained" color="primary">
-                    <p className={classes.txt}>6</p>
-                  </Button>
-                </Paper>
-              </Grid>
-              <Grid item xs={3}>
-                <Paper className={classes.btn}>
-                  <Button variant="contained" color="primary">
-                    <p className={classes.txt}>-</p>
-                  </Button>
-                </Paper>
-              </Grid>
-            </Grid>
-            <Grid container spacing={3}>
-              <Grid item xs={3}>
-                <Paper className={classes.btn}>
-                  <Button variant="contained" color="primary">
-                    <p className={classes.txt}>1</p>
-                  </Button>
-                </Paper>
-              </Grid>
-              <Grid item xs={3}>
-                <Paper className={classes.btn}>
-                  <Button variant="contained" color="primary">
-                    <p className={classes.txt}>2</p>
-                  </Button>
-                </Paper>
-              </Grid>
-              <Grid item xs={3}>
-                <Paper className={classes.btn}>
-                  <Button variant="contained" color="primary">
-                    <p className={classes.txt}>3</p>
-                  </Button>
-                </Paper>
-              </Grid>
-              <Grid item xs={3}>
-                <Paper className={classes.btn}>
-                  <Button variant="contained" color="primary">
-                    <p className={classes.txt}>+</p>
-                  </Button>
-                </Paper>
-              </Grid>
-            </Grid>
-            <Grid container spacing={3}>
-              <Grid item xs={3}>
-                <Paper className={classes.btn}>
-                  <Button variant="contained" color="primary">
-                    <p className={classes.txt}>0</p>
-                  </Button>
-                </Paper>
-              </Grid>
-              <Grid item xs={3}>
-                <Paper className={classes.btn}>
-                  <Button variant="contained" color="primary">
-                    <p className={classes.txt}>.</p>
-                  </Button>
-                </Paper>
-              </Grid>
-              <Grid item xs={6}>
-                <Paper className={classes.btn}>
-                  <Button style={{width: '100%'}} variant="contained" color="primary">
-                    <p className={classes.txt}>=</p>
-                  </Button>
-                </Paper>
-              </Grid>
-            </Grid>
-          </div>
-          <p style={{color: 'gray', marginBottom: '5px', fontSize: '13px', fontFamily: 'Orbitron'}}>By Michael Parra</p>
-        </Typography>
-      </div>
-    </Container>
-  </React.Fragment>
-  </div>
+            </div>
+            <p style={{color: 'gray', marginBottom: '5px', fontSize: '13px', fontFamily: 'Orbitron'}}>By Michael Parra</p>
+          </Typography>
+        </div>
+      </Container>
+      </React.Fragment>
+    </div>
   );
 }
 
